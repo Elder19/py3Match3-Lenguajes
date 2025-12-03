866f52da9f1f80fb25e0e46294b2673ff1388116
Elder19 <colegioempresarial20@gmail.com>
2025-12-03 15:24:56 -0600

    cambio de puert

--- Programa/db/Server.js
+++ Programa/db/Server.js
@@ -119,6 +119,8 @@ app.get("/api/ranking", obtenerRanking);
    ==================== INICIAR SERVER ==================
    ====================================================== */
 
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
